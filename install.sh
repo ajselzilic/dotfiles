36b96a7e166ad6e689981c75fa39a812ec488a12
Ajsel <ajselzilic@yahoo.com>
2026-03-20 19:58:06 +0100

    skip nix install if already present

--- install.sh
+++ install.sh
@@ -5,10 +5,17 @@ set -e
 if [ ! -f /nix/var/nix/profiles/default/bin/nix ]; then
   curl -L https://nixos.org/nix/install | sh -s -- --daemon
 fi
+
+# 2. Ensure Nix daemon is running
+if ! /nix/var/nix/profiles/default/bin/nix store ping > /dev/null 2>&1; then
+  sudo launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+  sleep 3
+fi
+
 source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
-# 2. Install chezmoi and apply dotfiles
+# 3. Install chezmoi and apply dotfiles
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply ajselzilic
 
-# 3. Bootstrap nix-darwin
-nix run nix-darwin -- switch --flake ~/.config/nix#macbookpro
+# 4. Bootstrap nix-darwin
+sudo HOME=$HOME nix run nix-darwin -- switch --flake ~/.config/nix#macbookpro
