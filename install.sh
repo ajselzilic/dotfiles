c7137249e59973e9f379ab67a68a90b7effc8cee
Ajsel <ajselzilic@yahoo.com>
2026-03-20 19:46:38 +0100

    add bootstrap install script

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 set -e
 
 # 1. Install Nix only if not already installed
-if ! command -v nix > /dev/null 2>&1; then
+if [ ! -f /nix/var/nix/profiles/default/bin/nix ]; then
   curl -L https://nixos.org/nix/install | sh -s -- --daemon
-  source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-else
-  echo "Nix already installed, skipping..."
 fi
+source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 # 2. Install chezmoi and apply dotfiles
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply ajselzilic
